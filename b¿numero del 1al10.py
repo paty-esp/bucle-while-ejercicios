79caf124ef45064d5c19ba3767ad3de95bb1c74f
paty-esp <patricia.espinola207@gmail.com>
2026-05-05 17:37:20 -0400

    Add files via upload

---
+++ b¿numero del 1al10.py
@@ -0,0 +1,8 @@
+while True:
+    num = int(input("ingresa un numero del 1 al 10:"))
+    if  1 >=  num <=10:
+        print("numero valido",num)
+        break
+    else:
+        print(" numero invalido")
+        continue
